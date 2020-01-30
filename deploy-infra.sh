320780e42dd77e36b0e027b09e96ee88e8b47d3d
Bryan Wilson <cubefive@gmail.com>
2020-01-30 06:49:37 -0500

    debuging codedeploy not finding appstop script

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -3,12 +3,17 @@
 STACK_NAME=awsbootstrap
 REGION=us-east-1 
 CLI_PROFILE=awsbootstrap
-
 EC2_INSTANCE_TYPE=t2.micro
 
+# Generate a personal access token with repo and admin:repo_hook 
+# permissions from https://github.com/settings/tokens 
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo) 
+GH_BRANCH=master
 
-AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
-     --query "Account" --output text`
+
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
 # Deploys static resources
@@ -34,7 +39,12 @@ aws cloudformation deploy \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-        EC2InstanceType=$EC2_INSTANCE_TYPE
+        EC2InstanceType=$EC2_INSTANCE_TYPE \
+        GitHubOwner=$GH_OWNER \
+        GitHubRepo=$GH_REPO \
+        GitHubBranch=$GH_BRANCH \
+        GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+        CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
