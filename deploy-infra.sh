9a2b3610c00838b9f68ee39b16f4285b4a855941
Bryan Wilson <cubefive@gmail.com>
2020-02-09 11:28:14 -0500

    Add HTTPS listener; Add cert to launch script

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -4,6 +4,8 @@ REGION=us-east-1
 CLI_PROFILE=awsbootstrap
 EC2_INSTANCE_TYPE=t2.micro
 DOMAIN=bwilson.tech
+CERT=`aws acm list-certificates --region $REGION --profile $CLI_PROFILE --output text \
+        --query "CertificateSummaryList[?DomainName=='$DOMAIN'].CertificateArn | [0]"`
 
 # Generate a personal access token with repo and admin:repo_hook 
 # permissions from https://github.com/settings/tokens 
@@ -60,6 +62,7 @@ aws cloudformation deploy \
     --parameter-overrides \
         EC2InstanceType=$EC2_INSTANCE_TYPE \
         Domain=$DOMAIN \
+        Certificate=$CERT \
         GitHubOwner=$GH_OWNER \
         GitHubRepo=$GH_REPO \
         GitHubBranch=$GH_BRANCH \
